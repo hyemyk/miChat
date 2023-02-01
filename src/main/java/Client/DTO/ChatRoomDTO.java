ea4ef530a9876b793238a497deb8796909182611
NC590 <sumin8611@gmail.com>
2023-02-01 17:40:47 +0900

    02/01 17:37 복구

---
+++ src/main/java/Client/DTO/ChatRoomDTO.java
@@ -0,0 +1,97 @@
+package Client.DTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatRoomDTO {
+    private List<UserDTO> inviteeList; // 방 참가자 리스트
+    private String roomName; // 방 이름
+
+
+    public ChatRoomDTO(String roomName) { // 유저가 방을 만들때 생성자
+        this.roomName = roomName;
+    }
+
+//
+//    public ChatRoomDTO(List<UserDTO> users) { // 유저 리스트가 방을 생성할 /**/
+//        this.inviteeList = users; // 유저리스트 복사
+//
+//        // 룸 입장
+//        for(UserDTO user : users){
+//            user.enterRoom(this);
+//        }
+//
+//    }
+
+//    public void enterUser(UserDTO user) {
+//        user.enterRoom(this);
+//        inviteeList.add(user);
+//    }
+//
+//    public void enterUser(List<UserDTO> users) { /**/
+//        for(UserDTO user : users){
+//            user.enterRoom(this);
+//        }
+//        inviteeList.addAll(users);
+//    }
+
+//
+
+
+    // 게임 로직
+
+    /**
+     * 해당 byte 배열을 방의 모든 유저에게 전송
+     * @param data 보낼 data
+     */
+    public void broadcast(byte[] data) {
+        for (UserDTO user : inviteeList) { // 방에 속한 유저의 수만큼 반복
+            // 각 유저에게 데이터를 전송하는 메서드 호출~
+            // ex) user.SendData(data);
+
+//			try {
+//				user.sock.getOutputStream().write(data); // 이런식으로 바이트배열을 보낸다.
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+        }
+    }
+
+
+
+
+
+    public String getRoomName() { // 방 이름을 가져옴
+        return roomName;
+    }
+
+    public int getUserSize() { // 유저의 수를 리턴
+        return inviteeList.size();
+    }
+
+
+    public List getInviteeList() {
+        return inviteeList;
+    }
+
+    public void setInviteeList(List inviteeList) {
+        this.inviteeList = inviteeList;
+    }
+
+/*
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ChatRoomDTO chatRoom = (ChatRoomDTO) o;
+
+        return id == chatRoom.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }*/
+}
---
+++ src/main/java/Client/DTO/UserDTO.java
@@ -0,0 +1,72 @@
+package Client.DTO;
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class UserDTO {
+    private String id;	// 아이디
+    private ArrayList<ChatRoomDTO> roomList;  // 유저가 속한 룸 List
+    private Socket socket; // 소켓 object
+
+    public UserDTO() { // 아무런 정보가 없는 깡통 유저를 만들 때
+    }
+
+    public UserDTO(String id) { // 아이디 정보만 가지고 생성
+        this.id = id;
+    }
+
+//    /**
+//     * 방에 입장시킴
+//     * room  입장할 방
+//     */
+//    public void enterRoom(ChatRoomDTO room) {
+//        room.enterUser(this); // 룸에 입장시킨 후
+//        this.room = room; // 유저가 속한 방을 룸으로 변경한다.(중요)
+//    }
+
+
+    public String getId() {
+        return id;
+    }
+
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+
+    public ArrayList<ChatRoomDTO> getRoomList() {
+        return roomList;
+    }
+
+    public void setRoomList(ArrayList<ChatRoomDTO> roomList) {
+        this.roomList = roomList;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+
+    /*
+        equals와 hashCode를 override 해줘야, 동일유저를 비교할 수 있다
+        비교할 때 -> gameUser 간 equals 비교, list에서 find 등
+     */
+ /*   @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GameUser gameUser = (GameUser) o;
+
+        return id == gameUser.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }*/
+
+}
